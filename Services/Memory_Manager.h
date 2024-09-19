3b20116cb7d5551f9acd49c43500809637ec41d0
delight-ed <150934645+delight-ed@users.noreply.github.com>
2024-09-19 22:06:50 +0200

    Adding new services

---
+++ Services/Memory_Manager.h
@@ -0,0 +1,6 @@
+#ifndef MEMORY_MANAGER_H
+#define MEMORY_MANAGER_H
+
+void 
+
+#endif
--- Services/SDL_Window_Manager.c
+++ Services/SDL_Window_Manager.c
@@ -22,34 +22,46 @@ Return: SDL_Window
 
 Parameters:
   char* title: Title of the newly created window
+  int x: Window's horizontal starting position or corresponding flags
+  int y: Window's vertical starting position or corresponding flags
   unsigned int width: Width dimension of the created window
   unsigned int height: Height dimension of the created window
+  Uint32 flags: Window's general flags with supported sub-systems
   SDL_Window** dock: Dock tracking all created windows. Must be resizable.
 
   Dvelopper mode:
   force: Force initialization of SDL through the improper canal. Can lead to errors.
 */
 SDL_Window *create_window(char* title, int x, int y, unsigned int width, unsigned int height, Uint32 flags, SDL_Window** dock, int force) {
+	
 	////CHECKING REQUIREMENTS
 	//Checking if SDL is initialized
 	if (!SDL_WasInit(SDL_INIT_VIDEO)) {
-		//Force through the Canal Error
-		if (force) {
+		if (!force) {
+			fprintf(stderr, "Error (create_window): SDL has not been initialized. Cancelling request of window creation. Make sure the proper initialization functions have been called beforehand.\nUse --force to force the initialization of SDL.\n\nRequest details:\ntitle: %s\nwidth:%d\nheight%d\n");
+		}
+		//FORCE: Dump the main canal and uses force canal instead.
+		else {
 			if(SDL_Init(SDL_INIT_VIDEO)!=0) {
 				//Exiting with error to avoid backtracking bugs
 				errx(1, "SDL Initializing failed in: create_window.");
 			}
 		}
-		else
-			fprintf(stderr, "Error (create_window): SDL has not been initialized. Cancelling request of window creation. Make sure the proper initialization functions have been called beforehand.\nUse --force to force the initialization of SDL.\n\nRequest details:\ntitle: %s\nwidth:%d\nheight%d\n");
-	}
+
 
 	////Initializing variables:
 	//window: element to return and add to the tracking dock
 	SDL_Window *window = NULL;
 
+	
 	//Creating the window
 	window = SDL_CreateWindow(title, x, y, width, height, flags);
+
+	//Tracking the window for future memory management: adding it to the dock
+	add_to(dock);
+
+	//Returning the currently created window
+	return window;
 }
 
 
