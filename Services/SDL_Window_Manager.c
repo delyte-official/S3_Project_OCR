82344839098e38ba6c1faa5e11d1cd8115934b79
delight-ed <nelson.rudent@eptia.fr>
2024-09-20 00:32:22 +0200

    Modifying the architecture to be simpler for use: add after, do not predict what is needed.

--- Services/SDL_Window_Manager.c
+++ Services/SDL_Window_Manager.c
@@ -1,9 +1,37 @@
+/*
+
+      ##############################################################
+      #                                                            #
+      #                    SDL_Window_Manager.c                    #
+      #                                                            #
+      #         Contains all functions related to handling,        #
+      #   modifying and supervising memory of SDL windows system.  #
+      #
+      ##############################################################
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
 ////HEADERS Files
 //Integrated C Libraries
 
 
 //SDL Libraries
 #include <SDL2/SDL.h>
+
+
+//Project Headers
+
 
 
 
@@ -14,71 +42,58 @@
 
 
 /*  create_window();
-Description:
-  Creates a window with SDL, stores it inside the given
-  window tracking dock and returns the window.
+  Creates a window with SDL and returns the window.
 
-  It is assumed that the tracking dock is empty or holding a bin
-  information and that the newly created window serves as the main
-  window of the project.
-
-Return: SDL_Window
-
-Parameters:
-  char* title: Title of the newly created window
-  int x: Window's horizontal starting position or corresponding flags
-  int y: Window's vertical starting position or corresponding flags
-  unsigned int width: Width dimension of the created window
-  unsigned int height: Height dimension of the created window
-  Uint32 flags: Window's general flags with supported sub-systems
-  SDL_Window* dock: Dock tracking all created windows. Must be resizable.
-
-  Dvelopper mode:
-  force: Force initialization of SDL through the improper canal. Can lead to errors.
+Requisites assumed:
+  No other window must be running by the program.
+  Returning window is the main project window.
+  SDL and its sub-systems must be initialized beforehand.
 */
-SDL_Window *create_window(char* title, int x, int y, unsigned int width, unsigned int height, Uint32 flags, SDL_Window* dock, int force) {
+SDL_Window *create_window( //Parameters:
+      char* title, //Title of the window
+      int x, //Horizontal starting position or corresponding flags
+      int y, //Vertical starting position or corresponding flags
+      unsigned int width, //Width of the window
+      unsigned int height, //Height of the window
+      Uint32 flags) { //General flags of the window
 
-	////CHECKING REQUIREMENTS
-	//Checking if SDL is initialized
-	if (!SDL_WasInit(SDL_INIT_VIDEO)) {
-		if (!force) {
-			fprintf(stderr, "Error (create_window): SDL has not been initialized. Cancelling request of window creation. Make sure the proper initialization functions have been called beforehand.\nUse --force to force the initialization of SDL.\n\nRequest details:\ntitle: %s\nwidth:%d\nheight%d\n");
-		}
-		//FORCE: Dump the main canal and uses force canal instead.
-		else {
-			if(SDL_Init(SDL_INIT_VIDEO)!=0) {
-				//Exiting with error to avoid backtracking bugs
-				errx(1, "SDL Initializing failed in: create_window.");
-			}
-		}
+  //window: element to return
+  SDL_Window *window = NULL;
 
+  //Creating the window
+  window = SDL_CreateWindow(title, x, y, width, height, flags);
 
-	////Initializing variables:
-	//window: element to return and add to the tracking dock
-	SDL_Window *window = NULL;
+  //Checking for success
+  if (!window)
+    errx(EXIT_FAILURE, "create_window: Window creation request failed.");
 
-	
-	//Creating the window
-	window = SDL_CreateWindow(title, x, y, width, height, flags);
-
-	//Tracking the window for future memory management: adding it to the dock
-	add_to(dock);
-
-	//Returning the currently created window
-	return window;
+  //Returning the created window
+  return window;
 }
 
 
-SDL_Renderer *create_renderer(SDL_Window *window) {
-	
+SDL_Renderer *create_renderer(SDL_Window *window, Uint32 flags) {
+  //renderer: element to return
+  SDL_Renderer *renderer = NULL;
+
+  //Creating the renderer
+  renderer = SDL_CreateRenderer(window, -1, flags);
+
+  //Checking for success
+  if (!renderer)
+    errx(EXIT_FAILURE, "create_renderer: Renderer creation request failed.");
+
+  //Returning the created renderer
+  return renderer;
 }
 
 
-void close_window(SDL_Window *window, SDL_Renderer *renderer) {
-	
-}
 
-
-void close_program(SDL_Window** windows, SDL_Renderer** renderers) {
-	
+void close_program(SDL_Window* window, SDL_Renderer* renderer) {
+  //Destroy the renderer
+  SDL_DestroyRenderer(renderer);
+  //Destroy the main window (assumed to be the only one created)
+  SDL_DestroyWindow(window);
+  //Close the program (assumed all memory usage has been freed)
+  SDL_QUIT();
 }
