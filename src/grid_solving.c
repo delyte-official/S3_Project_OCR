20a0daebe26360db47a05ec5706cc27482da85c8
Unknono <noefrancillard@gmail.com>
2024-10-13 01:55:34 +0200

    First push to the grid_detect branch. Testing if access to the Git repo was granted, and add the grid_solver.c which will contain all code about location and sequencing of grid, letters and words. Also added .vscode/ folder to be able to write in the file with Visual Studio Code, and of course added .vscode/ into the .gitignore.

---
+++ src/grid_solving.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_pixels.h>
+#include <SDL2/SDL_surface.h>
+#include <SDL2/SDL_error.h>
+#include <SDL2/SDL_image.h>
+#include <err.h>
+
+int main()
+{
+    
+}
