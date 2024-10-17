44a68366812cd6637f2d0fee98c1ffc1805f338c
Unknono <noefrancillard@gmail.com>
2024-10-18 00:51:17 +0200

    Created the first files of the Extraction part of the OCR project. No functions are created yet.

---
+++ src/Extraction/grid_detect.h
@@ -0,0 +1,15 @@
+#ifndef GRID_DETECT_H
+#define GRID_DETECT_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <err.h>
+#include <gtk/gtk.h>
+
+/*
+    Names of the functions used in grid_detect.c :
+    ---TO BE PRECISED SOON---
+*/
+
+#endif
