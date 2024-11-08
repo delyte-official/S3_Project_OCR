7b68fb44c715ad830f3563173b274b7dfc7720f8
delight-ed <150934645+delight-ed@users.noreply.github.com>
2024-11-08 17:20:39 +0100

    pushing work done

---
+++ src/Extraction/Detection.c
@@ -0,0 +1,186 @@
+#include <err.h>
+#include <gtk/gtk.h>
+#include <stdio.h>
+
+//Cluster struct
+typedef struct cluster {
+    int size;
+    int centerX, centerY;
+    int maxX, maxY, minX, minY;
+    struct cluster *next;
+} Cluster;
+
+
+/* is_black():
+    Returns if a pixel is black.
+*/
+int is_black(guchar *p) {
+    return (p[0] == 0) && (p[1] == 0) && (p[2]==0);
+}
+
+/* dfs():
+    Adds pixels to an initialized cluster.
+*/
+void dfs(Cluster *end, int x, int y, gboolean** visit,
+        GdkPixbuf *pixbuf) {
+    end->size++;
+    if (x > end->maxX)
+        end->maxX = x;
+    else if (x < end->minX)
+        end->minX = x;
+    if (y > end->maxY)
+        end->maxY = y;
+    else if (y < end->minY)
+        end->minY = y;
+    //Marking visited
+    visit[x][y] = TRUE;
+    //Recursive calls
+    int height = gdk_pixbuf_get_height(pixbuf);
+    int width = gdk_pixbuf_get_width(pixbuf);
+    guchar* pixels = gdk_pixbuf_get_pixels(pixbuf);
+    int N = gdk_pixbuf_get_n_channels(pixbuf);
+    int rowstride = gdk_pixbuf_get_rowstride(pixbuf);
+    for (int i = 0; i < 8; i++) { //Calculating new coordinates
+        int new_x = i < 3 ? x-1 : i<5 ? x : x+1;
+        int new_y = i < 3 ? y-1+i%3 : i<5 ? y+1+(i-4)*2: y-1+(i-2)%3;
+        if (new_x < 0 || new_y < 0 || new_y >= height ||new_x >= width)
+            continue;
+        guchar *p = pixels + new_x * N + new_y*rowstride;
+        if (visit[new_x][new_y] || !is_black(p))
+            continue;
+        dfs(end, new_x, new_y, visit, pixbuf);
+    }
+}
+
+
+/* initCluster():
+    Initialize a new cluster at the given pointer.
+*/
+void initCluster(Cluster **last, int x, int y, gboolean** visit,
+        GdkPixbuf *pixbuf) {
+    Cluster end = (Cluster) {
+        .size = 0,
+        .centerX = 0, .centerY = 0,
+        .maxX = x, .minX = x,
+        .maxY = y, .minY = y,
+        .next = NULL
+    };
+    if (*last == NULL)
+        *last = &end;
+    else
+        (*last)->next = &end;
+    *last = &end;
+    dfs(&end, x, y, visit, pixbuf);
+    end.centerX = (end.minX + end.maxX)/2;
+    end.centerY = (end.minY + end.maxY)/2;
+}
+
+
+/* retrieve_clusters():
+    First step of the extraction.
+    Retrieves all clusters of an image.
+*/
+int retrieve_clusters(GdkPixbuf *input, Cluster **last, int *median) {
+    //Grab the size of the pixbuf
+    int width = gdk_pixbuf_get_width(input);
+    int height = gdk_pixbuf_get_height(input);
+
+    //Array to mark pixels as visisted
+    gboolean** visited = malloc(width * sizeof(gboolean*));
+    if (visited == NULL)
+        errx(EXIT_FAILURE, "malloc()");
+    for (int i = 0; i < width; i++) {
+        visited[i] = calloc(height, sizeof(gboolean));
+        if (visited[i] == NULL) {
+            free(visited);
+            errx(EXIT_FAILURE, "calloc()");
+        }
+    }
+
+    //Retrieving data form the input pixbuf
+    int N = gdk_pixbuf_get_n_channels(input);
+    int rowstride = gdk_pixbuf_get_rowstride(input);
+    guchar* pixels = gdk_pixbuf_get_pixels(input);
+    int clusterCount = 0;
+
+    //Finding all clusters
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            //new cluster?
+            guchar *pixel = pixels + y * rowstride + x * N;
+            if (is_black(pixel) && !visited[x][y]) {//0 means a black pixel
+                initCluster(last, x, y, visited, input);
+                *median+=(*last)->size;
+                clusterCount++;
+            }
+        }
+    }
+    //Updating median
+    *median/=clusterCount;
+    return clusterCount;
+}
+
+
+//JUST TO TEST - TO REMOVE
+void testing(Cluster *first, int size, GdkPixbuf *pixbuf) {
+    GdkPixbuf *res = gdk_pixbuf_copy(pixbuf);
+    
+    guchar* pixels = gdk_pixbuf_get_pixels(res);
+    int N = gdk_pixbuf_get_n_channels(res);
+    int rowstride = gdk_pixbuf_get_rowstride(res);
+
+    guchar *p;
+    //Iterating over the clusters
+    while (first!=NULL) {
+        //Coloring into red the whole cluster
+        for (int x = first->minX; x <= first->maxX; x++) {
+            p = pixels + first->minY*rowstride +x*N;
+            p[0] = 255;p[1]=0;p[2]=0;
+            p = pixels + first->maxY*rowstride +x*N;
+            p[0] = 255;p[1]=0;p[2]=0;
+        }
+        for (int y = first->minY; y <= first->maxY; y++) {
+            p = pixels + y*rowstride +first->minX*N;
+            p[0] = 255;p[1]=0;p[2]=0;
+            p = pixels + y*rowstride +first->maxX*N;
+            p[0] = 255;p[1]=0;p[2]=0;
+        }
+        printf("there\n");
+        first=first->next;
+        printf("or there\n");
+    }
+    gdk_pixbuf_save(res, "save.png", "png", NULL, NULL);
+}
+
+
+/* threshold_filer():
+    Filter all clusters according to a threshold.
+*/
+int threshold_filter(Cluster *clusters, int count, int median) {
+    
+    return count;
+}
+
+
+/* extract_information():
+    Performs all the extraction from an image.
+*/
+void extract_information(GdkPixbuf *input) {
+    //Linked list of clusters
+    Cluster *start = NULL;
+    
+    //STEP 1: retrieving EVERY clusters
+    int median_size = 0;
+    int count = retrieve_clusters(input, &start, &median_size);
+    printf("Median is: %d\n", median_size);
+    //STEP 2: Filter clusters
+    //count = threshold_filter(start, count, median_size);
+
+    //TESTING
+    testing(start,count, input);
+}
+
+int main() {
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file("input.png", NULL);
+    extract_information(pixbuf);
+}
